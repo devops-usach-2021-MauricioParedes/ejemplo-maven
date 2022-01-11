efdaaab55e7b71bc0436756e44124d7a8f0f68ec
Mauricio P <mauricioparedesf@gmail.com>
2022-01-10 23:09:18 -0300

    Se agregan metodos y test unitarios

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,5 +1,9 @@
 package com.devopsusach2020;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.devopsusach2020.service.Parseador;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,49 @@ class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	private final Parseador parseador = new Parseador();
+
+	@Test
+	public void obtenerParte1Unidades() {
+
+		String ecuacion1 = "2x - 1 = 0";
+
+		int resultado = parseador.obtenerParte1(ecuacion1);
+
+		assertEquals(2, resultado);
+	}
+
+
+	@Test
+	public void obtenerParte2Suma() {
+
+		String ecuacion1 = "2x + 1 = 0";
+
+		int resultado = parseador.obtenerParte2(ecuacion1);
+
+		assertEquals(1, resultado);
+	}
+
+	@Test
+	public void obtenerParte3Positivo() {
+
+		String ecuacion1 = "2x + 1 = 3";
+
+		int resultado = parseador.obtenerParte3(ecuacion1);
+
+		assertEquals(3, resultado);
+	}
+
+	@Test
+	public void obtenerOperadorSuma() {
+
+		String ecuacion2 = "2x + 1 = 0";
+
+		String operador = parseador.obtenerOperador(ecuacion2);
+
+		assertEquals("+", operador);
 	}
 
 }
